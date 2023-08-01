258e6b779563b129066717be591a4f5668f462e0
quandha <quandha1009@gmail.com>
2023-08-01 22:28:14 +0700

    refractor apollo server

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,18 +1,3 @@
-import { createSchema, createYoga } from "graphql-yoga";
-import { typeDefs } from "@/apollo/graphql-schema";
-import { resolvers } from "@/apollo/resolvers";
-
-const { handleRequest } = createYoga({
-  schema: createSchema({
-    typeDefs,
-    resolvers,
-  }),
-
-  // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
-  graphqlEndpoint: "/api/graphql",
-
-  // Yoga needs to know how to create a valid Next response
-  fetchAPI: { Response },
-});
+import handleRequest from "@/apollo/apollo-server";
 
 export { handleRequest as GET, handleRequest as POST };
