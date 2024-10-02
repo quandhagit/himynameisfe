9b0637d19bb6bd59ccd88b0a278cc53ef46e6353
quandha <quandha1009@gmail.com>
2024-10-02 22:01:14 +0700

    fix

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -5,13 +5,21 @@ import { auth } from "@/config/firebase";
 import SendEmailVerification from "@/feature/auth/SendEmailVerification";
 import VerifyEmail from "@/feature/auth/VerifyEmail";
 import { useAuthContext } from "@/provider/AuthProvider";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 const VerifyEmailPage: React.FC = () => {
   const searchParams = useSearchParams();
   const { isLoading } = useAuthContext();
+  const route = useRouter();
   const mode = searchParams.get("mode");
   const oobCode = String(searchParams.get("oobCode"));
+
+  useEffect(() => {
+    if (auth.currentUser?.emailVerified) {
+      route.push("/");
+    }
+  }, [auth.currentUser?.emailVerified]);
 
   if (!mode && (isLoading || auth.currentUser?.emailVerified)) {
     return <></>;
